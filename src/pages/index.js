89593a8cd87b31f27d7218348d147529caa0b196
gak <gak@gak0.com>
2021-03-23 17:34:25 +1100

    yeah better keep it

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,6 +76,16 @@ export default function Home() {
                 <div className="container">
                     <h1 className="hero__title">{siteConfig.title}</h1>
                     <p className="hero__subtitle">{siteConfig.tagline}</p>
+                    <div className={styles.buttons}>
+                        <Link
+                            className={clsx(
+                                'button button--lg',
+                                styles.getStarted,
+                            )}
+                            to={useBaseUrl('docs/')}>
+                            Introduction
+                        </Link>
+                    </div>
                 </div>
             </header>
             <main>
