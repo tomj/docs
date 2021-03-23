4e0a5ff923fc9d9f11497d0eb15ea6872dbc6563
gak <gak@gak0.com>
2021-03-23 17:33:37 +1100

    get rid of silly button

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,16 +76,6 @@ export default function Home() {
                 <div className="container">
                     <h1 className="hero__title">{siteConfig.title}</h1>
                     <p className="hero__subtitle">{siteConfig.tagline}</p>
-                    <div className={styles.buttons}>
-                        <Link
-                            className={clsx(
-                                'button button--outline button--lg',
-                                styles.getStarted,
-                            )}
-                            to={useBaseUrl('docs/')}>
-                            Introduction
-                        </Link>
-                    </div>
                 </div>
             </header>
             <main>
